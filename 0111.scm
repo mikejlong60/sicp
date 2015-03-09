cc860bd6aa6970618827aab7ec051457c1137b58
mikejlong60 <mjlong@functionicity.com>
2015-03-09 09:31:43 -0400

    Adds second half of 0111. But its not correct yet.

--- 0111.scm
+++ 0111.scm
@@ -5,3 +5,18 @@
                (* (fr (- n 3)) 3)
                )
     )))
+
+
+(define (f n)
+    (fi-iter 0 n))
+
+;Not correct yet
+(define (fi-iter result n)
+    (if (< n 3)
+        result
+        (fi-iter (+ (- n 1) (* (- n 2) 2) (* (- n 3) 3)) (- n 1))
+    ))
+
+
+(define (testf n)
+    (= (f n) (fr n)))
