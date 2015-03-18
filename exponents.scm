a23099dbe10f33f7899770e2c993680bf1add11c
mikejlong60 <mjlong@functionicity.com>
2015-03-18 15:47:12 -0400

    Adds exponents procedure from book.

---
+++ exponents.scm
@@ -0,0 +1,15 @@
+(define (expt b n)
+    (if (= n 0)
+        1
+        (* b (expt b (- n 1)))))
+
+
+(define (expti b n)
+    (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+    (if (= counter 0)
+        product
+        (expt-iter b
+           (- counter 1)
+           (* b product))))
